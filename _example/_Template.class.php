e55177ed38f6405c94dffe7c40e004edde166bd9
Kim Jong Kwan <apmsoft@gmail.com>
2014-11-03 15:35:10 +0900

    기본작업 수정
    include js 파일 등록

--- _example/_Template.class.php
+++ _example/_Template.class.php
@@ -21,7 +21,7 @@ for($i=0; $i<3; $i++){
 
 # tpl 변수
 $tpl['strings'] = array(
-    'name'=>'이름',
+    'app_name'=>'템플릿 테스트',
     'template'=>'fancy-up-php-template'
 );
 
@@ -29,7 +29,10 @@ $tpl['items'] =$items;
 
 # prints
 $tpl->compile_dir =_ROOT_PATH_.'/'._TPL_;
-$tpl->compile     = true;   # 강제컴파일여부
-$tpl->compression = false;  # 압축(min)여부
-echo $tpl->display();
+$tpl->chgimgpath  = true;       # 이미지경로 자동 변경 기능 (기본 true)
+$tpl->compile     = true;       # 강제 컴파일 여부 [개발이 완료되면 false로 전환하세요] (기본 true)
+$tpl->compression = false;      # 압축(min)여부 (기본 true)
+$tpl->gzlib_compression = true; # gzib 압축여부(gzib 라이브러리 설치 서버에서만 사용가능) (기본 true)
+$tpl->display();                # gzib 출력이 가능할 경우 (기본 return String)
+# echo $tpl->display();         # gzib 출력이 안될 경우
 ?>
